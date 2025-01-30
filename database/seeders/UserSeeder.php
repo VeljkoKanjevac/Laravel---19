0dd5713cf3a840b59438ee4010118dfc53516552
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-01-30 23:05:52 +0100

    Added UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\WeatherModel;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $amount = $this->command->getOutput()->ask("Koliko korisnika zelite da napravite?", 500);
+
+        $password = $this->command->getOutput()->ask("Koju sifru zelite?", "123456");
+
+        $faker = Factory::create();
+
+        $this->command->getOutput()->progressStart($amount);
+
+        for ($i = 0; $i < $amount; $i++)
+        {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make($password),
+            ]);
+
+            $this->command->getOutput()->progressAdvance();
+        }
+        $this->command->getOutput()->progressFinish();
+    }
+}
