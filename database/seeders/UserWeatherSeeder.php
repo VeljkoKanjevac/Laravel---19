5c36dd18959fb90e3fc9d544a1f95308d5400478
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-05 17:44:09 +0100

    Optimized imports

--- database/seeders/UserWeatherSeeder.php
+++ database/seeders/UserWeatherSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\WeatherModel;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserWeatherSeeder extends Seeder
