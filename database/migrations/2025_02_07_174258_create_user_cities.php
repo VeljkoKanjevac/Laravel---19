c2826dacfa5445f85852662de0f65d6af0cc8ba6
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-07 18:53:31 +0100

    User Cities, migration and model

---
+++ database/migrations/2025_02_07_174258_create_user_cities.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_cities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('city_id');
+            $table->timestamps();
+
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_cities');
+    }
+};
