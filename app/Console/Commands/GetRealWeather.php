2cc7be82393850e2146d3ffa08d0ab1c283b4c71
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-10 00:28:02 +0100

    Added writing data in forecasts table from WeatherAPI

--- app/Console/Commands/GetRealWeather.php
+++ app/Console/Commands/GetRealWeather.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\CitiesModel;
+use App\Models\ForecastsModel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -28,9 +30,19 @@ class GetRealWeather extends Command
      */
     public function handle()
     {
+        $city = $this->argument('city');
+
+        $dbCity = CitiesModel::where(['name' => $city])->first();
+        if($dbCity === null)
+        {
+            $dbCity = CitiesModel::create([
+                'name' => $city,
+            ]);
+        }
+
         $response = Http::get(env("WEATHER_API_URL")."v1/forecast.json", [
             'key' => env('WEATHER_API_KEY'),
-            'q' =>  $this->argument('city'),
+            'q' =>  $city,
             'aqi' => 'no',
             'days' => 1,
         ]);
@@ -41,6 +53,27 @@ class GetRealWeather extends Command
             $this->output->error($jsonResponse['error']['message']);
         }
 
-        dd($jsonResponse);
+        if($dbCity->todayForecast !== null)
+        {
+            $this->output->comment("Command finished");
+            return;
+        }
+
+        $forecast_date = $jsonResponse['forecast']['forecastday'][0]['date'];
+        $temperature = $jsonResponse['forecast']['forecastday'][0]['day']['avgtemp_c'];
+        $weather_type = $jsonResponse['forecast']['forecastday'][0]['day']['condition']['text'];
+        $probability = $jsonResponse['forecast']['forecastday'][0]['day']['daily_chance_of_rain'];
+
+
+        $forecast = [
+            'city_id' => $dbCity->id,
+            'temperature' => $temperature,
+            'weather_type' => strtolower($weather_type),
+            'forecast_date' => $forecast_date,
+            'probability' => $probability,
+        ];
+
+        ForecastsModel::create($forecast);
+        $this->output->comment("Added new forecast!");
     }
 }
